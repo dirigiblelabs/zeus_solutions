7c40e862b489b259d8cbc8a4cc8f3c76687b1d95
delchev <delchevn@gmail.com>
2017-03-01 12:22:50 +0200

    ordering

--- zeus_solutions/ScriptingServices/zeus/solutions/extensions/cockpit_solutions.js
+++ zeus_solutions/ScriptingServices/zeus/solutions/extensions/cockpit_solutions.js
@@ -2,7 +2,7 @@
 /* eslint-env node, dirigible */
 
 const PATH = "/solutions";
-const HTML_LINK = "../../zeus/solutions/index.html";
+const HTML_LINK = "../solutions/index.html";
 
 //exports.getMenuItem = function() {
 //	return {  
@@ -16,6 +16,8 @@ exports.getSidebarItem = function() {
 	return {  
       "name": "Solutions",
       "path": PATH,
-      "link": HTML_LINK
+      "link": HTML_LINK,
+      "category": "Define",
+      "order": 102
    };
 };
